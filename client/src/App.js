95588b7a4cff0cffaec389b707b38c15c8ebc04c
aieshah <aieshah_nasir@hotmail.com>
2023-09-30 22:19:07 +0530

    add home

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-// import Header from './components/Header'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
